0fecc4780a8b77d4fc92cae03246d982f2216bd8
WendersonFontes <81767673+WendersonFontes@users.noreply.github.com>
2024-01-04 13:37:20 -0300

    Update antecessor_e_sucessor.py

--- antecessor_e_sucessor.py
+++ antecessor_e_sucessor.py
@@ -1,9 +1,4 @@
-# Forma 1
 n = int(input('Digite um número:'))
 a = n - 1
 s = n + 1
 print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, a, s))
-
-# Forma 2
-n = int(input('Digite um número:'))
-print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n - 1), (n + 1)))
