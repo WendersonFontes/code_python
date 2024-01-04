d3e6e56337ca6a6aee583544699e3e4b786fe3ff
WendersonFontes <81767673+WendersonFontes@users.noreply.github.com>
2024-01-04 13:45:37 -0300

    Create converter_temperatura.py

---
+++ converter_temperatura.py
@@ -0,0 +1,3 @@
+temperatura = float(input('Informe a temperatura em °C: '))
+conversao = (temperatura * 9/5) + 32
+print('A temperatura de {:.1f} corresponde a {:.1f}°F!'.format(temperatura, conversao))
