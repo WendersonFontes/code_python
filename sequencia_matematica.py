c80b78cc4c5731379d77d42ef574b36474f3d12e
WendersonFontes <81767673+WendersonFontes@users.noreply.github.com>
2024-01-04 13:33:55 -0300

    Create sequencia_matematica.py

---
+++ sequencia_matematica.py
@@ -0,0 +1,7 @@
+n1 = int(input('Pense um número:'))
+n2 = n1 * 2
+n3 = n2 + 50
+n4 = 50
+n5 = int(input('Divida por 2 o resultado da soma:'))
+s = ((n1 * 2 + n4) / 2) - n1
+print('Sequência de números: 1º - {} 2º - {} 3º - {} 4º - {} 5º - {} é igual a {}!'.format(n1, n2, n3, n4, n5, s))
