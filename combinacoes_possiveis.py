d98cf8a7faab79b82fc0675748ce320fc8cebb30
WendersonFontes <81767673+WendersonFontes@users.noreply.github.com>
2024-05-14 11:28:22 -0300

    Update combinacoes_possiveis.py

--- combinacoes_possiveis.py
+++ combinacoes_possiveis.py
@@ -1,12 +1,16 @@
 import itertools
 
-def generate_sequences():
+def generate_sequences(limit=10):
     numbers = list(range(1, 20))
     sequences = []
+    count = 0  # Inicializa um contador
 
     for combo in itertools.combinations(numbers, 5):
+        if count >= limit:
+            break  # Sai do loop quando o limite é atingido
         sequence = list(combo)
         sequences.append(sequence)
+        count += 1  # Incrementa o contador
 
     return sequences
 
